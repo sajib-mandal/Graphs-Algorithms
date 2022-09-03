a91f31a78cff7cb96fbd0494eed46648f8ce78cd
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-09-03 13:16:31 +0530

    Create recursive_has_path_dft.py

---
+++ Has Path/Undirected Path/recursive_has_path_dft.py
@@ -0,0 +1,43 @@
+def undirected_path(edges, node_A, node_B):
+    # create helper function to convert "edges_list --> adjacency_list
+    graph = build_graph(edges)
+    return has_path(graph, node_A, node_B, set())  # set gives me O(1) lookup and O(1) insertion
+
+
+def has_path(graph, src, dst, visited):
+    if src == dst:
+        return True
+
+    if src in visited:
+        return False
+    visited.add(src)
+    for neighbor in graph[src]:
+        if has_path(graph, neighbor, dst, visited) == True:
+            return True
+    return False
+
+
+def build_graph(edges):
+    graph = {}  # graph using dict
+    for edge in edges:
+        a, b = edge
+        if a not in graph:
+            graph[a] = []
+        if b not in graph:
+            graph[b] = []
+
+        graph[a].append(b)
+        graph[b].append(a)
+
+    return graph
+
+
+edges = [
+    ("i", "j"),
+    ("k", "i"),
+    ("m", "k"),
+    ("k", "l"),
+    ("o", "n")
+]
+
+print(undirected_path(edges, "j", "m"))
